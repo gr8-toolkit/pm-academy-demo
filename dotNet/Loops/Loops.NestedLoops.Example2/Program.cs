d2c5d51028cf2fa5cc674a45738a2af038625d0e
dmytro.maximov <dmytro.maximov@betlab.com>
2020-12-18 13:37:00 +0200

    Fix Loops.NestedLoops.Example2

--- dotNet/Loops/Loops.NestedLoops.Example2/Program.cs
+++ dotNet/Loops/Loops.NestedLoops.Example2/Program.cs
@@ -11,7 +11,7 @@ namespace Loops.NestedLoops.Example2
 
         private static void PrintPrime(int low, int high)
         {
-            for (var i = low; i <= high; i++)
+            for (var i = Math.Max(low, 2); i <= high; i++)
             {
                 var isPrime = true;
                 for (var j = 2; j < i; j++)
