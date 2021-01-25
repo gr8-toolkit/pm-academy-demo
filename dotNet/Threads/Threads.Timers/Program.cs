5cd11f7323a4feb4604cfed345ab457c79886d82
Vasyl Melnyk <vm4dev@gmail.com>
2021-01-25 18:58:31 +0200

    Merge with basic project. (#2)
    * Add dotNet Loops demo stuff
    
    * Fix Loops.NestedLoops.Example2
    
    * Add demo projects : Attributes, Files, Linq
    
    * dotNet : Add Serialization demo
    
    * Add testing demo
    
    * fix naming
    
    * chanhe folder`s path
    
    * Change path of folders
    
    * .Net 15. SQL (index, CRUD), PgSQL.
    
    * Add HomeWork5 content
    
    * [dotNet] : Add Threads demo stuff
    
    Co-authored-by: DmxDev <63281813+DmxDev@users.noreply.github.com>
    Co-authored-by: dmytro.maximov <dmytro.maximov@betlab.com>
    Co-authored-by: Illia Maier <64944310+Illia-M@users.noreply.github.com>
    Co-authored-by: khrystyna.terletska <khrystyna.terletska@pm.bet>
    Co-authored-by: Mykola Remeslennikov <mykola.remeslennikov@pm.bet>

---
+++ dotNet/Threads/Threads.Timers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads.Timers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Hello Timer! [{id}]");
+            
+            var t = new Timer(Callback, null, 0, 1_000);
+            
+            Console.WriteLine("Press enter to quit!");
+            Console.ReadLine();
+
+            t.Dispose();
+            Console.WriteLine("Good buy!");
+        }
+
+        private static void Callback(object state)
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"[Now] : {DateTime.Now:O} [{id}]");
+        }
+    }
+}
