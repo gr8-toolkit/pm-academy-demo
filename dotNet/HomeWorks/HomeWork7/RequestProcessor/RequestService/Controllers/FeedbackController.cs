9583ab09bbfb6f8ad2859ace5a646e8283c408eb
dmytro.maximov <dmytro.maximov@betlab.com>
2021-02-15 23:58:39 +0200

    [dotNet] : Extend Homework 7

---
+++ dotNet/HomeWorks/HomeWork7/RequestProcessor/RequestService/Controllers/FeedbackController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Memory;
+using RequestService.Models;
+
+namespace RequestService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IMemoryCache _cache;
+
+        public FeedbackController(IMemoryCache cache, ILogger<FeedbackController> logger)
+        {
+            _cache = cache;
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Feedback> Get(int id)
+        {
+            if (!_cache.TryGetValue(GetFeedbackKey(id), out var feedback))
+            {
+                return NotFound();
+            }
+
+            return (Feedback)feedback;
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Post(int id, Feedback feedback)
+        {
+            if (!ModelState.IsValid) return BadRequest("Model is invalid");
+            if (_cache.TryGetValue(GetFeedbackKey(id), out _))
+            {
+                return Conflict("Already exists");
+            }
+            
+            var cacheEntryOptions = new MemoryCacheEntryOptions();
+            _cache.Set(GetFeedbackKey(id), feedback, cacheEntryOptions);
+
+            return CreatedAtRoute("", new {id});
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!_cache.TryGetValue(GetFeedbackKey(id), out var feedback))
+            {
+                return NotFound("Feedback was not found");
+            }
+            _cache.Remove(GetFeedbackKey(id));
+
+            return NoContent();
+        }
+
+        private static string GetFeedbackKey(int id) => $"feedback_{id}";
+    }
+}
---
+++ dotNet/HomeWorks/HomeWork7/RequestProcessor/RequestService/Models/Feedback.cs
@@ -0,0 +1,8 @@
+﻿namespace RequestService.Models
+{
+    public class Feedback
+    {
+        public int Rate { get; set; }
+        public string Message { get; set; }
+    }
+}
