3eee7d67efb86831f28649ec22360f3769ca908c
dmytro.maximov <dmytro.maximov@betlab.com>
2021-02-16 00:02:48 +0200

    [dotNet] : Add AspNetCore Routing demo stuff

---
+++ dotNet/AspRouting/AspRouting.Data/IFeedService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AspRouting.Data
+{
+    public interface IFeedService
+    {
+        Task<string> GetItemAsync(int id);
+        Task<IEnumerable<string>> GetItemsAsync();
+        Task<int> AddItemAsync(string feedItem);
+        Task AddOrUpdateItemAsync(int id, string feedItem);
+        Task<bool> DeleteItemAsync(int id);
+        Task ClearAsync();
+    }
+}
---
+++ dotNet/AspRouting/AspRouting.Mappings/Startup.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspRouting.Mappings
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
