171b8e77d9e06650b9686e60aff9809292a155b2
Illia Maier <illia.maier@pm.bet>
2021-02-21 22:24:22 +0200

    Add more tests to StringMutatorTests

--- dotNet/HomeWorks/HomeWork8/DesignPatterns/DesignPatterns.UnitTests/StringMutatorTests.cs
+++ dotNet/HomeWorks/HomeWork8/DesignPatterns/DesignPatterns.UnitTests/StringMutatorTests.cs
@@ -22,5 +22,41 @@ namespace DesignPatterns.UnitTests
 
             Assert.Equal("GNIRTS  TUPNI  EMOS", actual);
         }
+
+        [Theory]
+        [InlineData("123,456", ",")]
+        [InlineData("123.456", ".")]
+        [InlineData("some 123number", "some number")]
+        [InlineData("123number", "number")]
+        [InlineData("text with 01234567890 0987654321 number", "text with   number")]
+        [InlineData("number123", "number")]
+        public void RemoveNumbersMutatorTest(string input, string expectedResult)
+        {
+            IStringMutator stringMutator = new RemoveNumbersMutator();
+
+            string actual = stringMutator.Mutate(input);
+
+            Assert.Equal(expectedResult, actual);
+        }
+
+        [Fact]
+        public void NullHandlingTest()
+        {
+            IStringMutator stringMutator = new ToUpperMutator();
+            var result = stringMutator.Mutate(null);
+            Assert.Null(result);
+
+            stringMutator = new InvertMutator();
+            result = stringMutator.Mutate(null);
+            Assert.Null(result);
+
+            stringMutator = new RemoveNumbersMutator();
+            result = stringMutator.Mutate(null);
+            Assert.Null(result);
+
+            stringMutator = new TrimMutator();
+            result = stringMutator.Mutate(null);
+            Assert.Null(result);
+        }
     }
 }
