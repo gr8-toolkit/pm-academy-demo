ec8f63995c69ab08d8b60641c2ef371f72014f52
DM <dmytro.maximov@pm.bet>
2021-11-07 20:01:43 +0200

    [dotnet][hw] : add homework0

---
+++ dotNet/HomeWorks/HomeWork0/Hw0.Exercise1/EchoApplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hw0.Exercise1
+{
+    /// <summary>
+    /// Echo application core.
+    /// </summary>
+    public class EchoApplication
+    {
+        /// <summary>
+        /// Runs echo application.
+        /// Prints <paramref name="args"/> to the output.
+        /// Arguments will be joined with the white spaces. 
+        /// New line separator will be added to the end of the resulted string.
+        /// </summary>
+        /// <param name="args">Echo arguments.</param>
+        /// <returns>Return <c>0</c> in case of success.</returns>
+        public int Run(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                var echo = string.Join(' ', args);
+                Console.WriteLine(echo);
+            }
+            return 0;
+        }
+    }
+}
---
+++ dotNet/HomeWorks/HomeWork0/Hw0.Tests/Tools/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hw0.Tests.Tools
+{
+    /// <summary>
+    /// String extension for unit tests.
+    /// </summary>
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Normalizes console output.
+        /// </summary>
+        public static string NormalizeOutput(this string str, bool normalizeCase = false)
+        {
+            if (str is null) return str;
+
+            // Trim whitespaces and NewLine chars
+            str = str.Trim().Trim(Environment.NewLine.ToCharArray());
+
+            // Convert to upper case
+            if (normalizeCase) str = str.ToUpperInvariant();
+            return str;
+        }
+    }
+}
